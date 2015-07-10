4fd9b9584dd1d273274be5734f783d0d4a857a5a
dorian-marchal <marchal.dorian@gmail.com>
2015-07-10 21:13:48 +0200

    Add the base project structure

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 'use strict';
 
+var options = {
+    repository: 'https://github.com/dorian-marchal/phonegap-boilerplate',
+    branch: 'master',
+};
+
+/**
+ * Check that the cli is used in a phonegap boilerplate project
+ * @return {bool} true if we are in a pb project, else otherwise
+ */
+var checkWorkingDirectory = function () {
+
+};
+
+/**
+ * Load the configuration from the config file.
+ * Prompt the user to fille the configuration file if it's missing.
+ */
+var loadConfig = function () {
+
+};
+
 module.exports = {
     fetch: function () {
 
