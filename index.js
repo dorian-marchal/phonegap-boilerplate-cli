36758e08567f2b28e272e69deb62b2fc0f03e569
dorian-marchal <marchal.dorian@gmail.com>
2015-07-12 19:41:48 +0200

    Move the configuration logic in a module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var fs = require('fs');
-var extend = require('extend');
+var ConfigManager = require('./ConfigManager');
 
 var PhonegapBoilerplate = function(workingDirectory) {
 
@@ -9,146 +9,21 @@ var PhonegapBoilerplate = function(workingDirectory) {
     throw new Error('Working directory not set');
   }
 
-  this.setWorkingDirectory(workingDirectory);
+  this.config = new ConfigManager();
+  this.setWorkingDirectory(workingDirectory)
+
 };
 
 PhonegapBoilerplate.prototype = {
   constructor: PhonegapBoilerplate,
 
-  configFile: 'pb-config.json',
-  options: {
-    repository: 'https://github.com/dorian-marchal/phonegap-boilerplate',
-    branch: 'master',
-  },
+  config: null,
 
   workingDirectory: null,
 
   setWorkingDirectory: function(workingDirectory) {
     this.workingDirectory = workingDirectory;
-  },
-
-  /**
-   * Set the config file path
-   * @param {String} configFile path relative to the working directory
-   */
-  setConfigFile: function(configFile) {
-    this.configFile = configFile;
-  },
-
-  /**
-   * Merge the given options into this.options
-   */
-  setOptions: function(options) {
-    extend(this.options, options);
-  },
-
-  /**
-   * Load the configuration from the config file.
-   * Prompt the user to fill the configuration file if it's missing and save it.
-   */
-  loadConfig: function(done) {
-
-    var that = this;
-    done = done || function() {};
-
-    var filePath = this.workingDirectory + '/' + this.configFile;
-
-    // Read from the config file
-    fs.open(filePath, 'r', function(err) {
-      // If the file doesn't exist, create it
-      if (err) {
-        done(err);
-      // else, read it
-      } else {
-        var userOptions;
-        try {
-          userOptions = require(filePath);
-          that.setOptions(userOptions);
-          done();
-        }
-        catch (err) {
-          done('Bad config file formatting: ' + err);
-        }
-      }
-    });
-  },
-
-  /**
-   * Prompt the user, asking for configuration vars
-   * @param {function} done Called with the config object when done
-   */
-  promptConfig: function(done) {
-    done = done || function() {};
-
-    var prompt = require('prompt');
-    prompt.message = '- ';
-    prompt.delimiter = '';
-
-    var schema = [
-      {
-        name: 'repository',
-        default: this.options.repository,
-        description: 'Phonegap Boilerlate remote repository:',
-      },
-      {
-        name: 'branch',
-        default: this.options.branch,
-        description: 'Remote Phonegap Boilerplate branch:',
-      },
-    ];
-
-    prompt.get(schema, function(err, userConfig) {
-      if (err) {
-        console.error('\nError: a config file must be created before using pb');
-        process.exit();
-      }
-      done(userConfig);
-    });
-  },
-
-  /**
-   * Persist current options in the config file
-   */
-  saveOptions: function(done) {
-
-    var that = this;
-    done = done || function() {};
-
-    var filePath = this.workingDirectory + '/' + this.configFile;
-
-    fs.open(filePath, 'w', function(err, fd) {
-      if (err) {
-        throw err;
-      } else {
-        fs.write(fd, JSON.stringify(that.options, null, '  '), function(err) {
-          if (err) {
-            throw 'Error writing config file: ' + err;
-          }
-
-          fs.close(fd, function() {
-            console.log('Config file written in: ' + filePath);
-            done();
-          });
-        });
-      }
-    });
-  },
-
-  /**
-   * Prompt the user, asking for configuration vars and save current options.
-   */
-  promptReconfigure: function(done) {
-
-    var that = this;
-    done = done || function() {};
-
-    this.loadConfig(function() {
-      that.promptConfig(function(options) {
-        that.setOptions(options);
-        that.saveOptions(done);
-      });
-    });
-
+    this.config.setWorkingDirectory(workingDirectory);
   },
 
   /**
@@ -162,7 +37,6 @@ PhonegapBoilerplate.prototype = {
    * verify that the pb remote exists
    */
   checkRemote: function() {
-
   },
 
   /**
