95968a3175da7897b065552b2c82e4a28dc50ca3
dorian-marchal <marchal.dorian@gmail.com>
2015-07-13 21:21:55 +0200

    Check if main files/folder exists in checkWorkingDirectory

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs');
+var async = require('async');
 var ConfigManager = require('./ConfigManager');
 
 var PhonegapBoilerplate = function(workingDirectory) {
@@ -10,7 +11,7 @@ var PhonegapBoilerplate = function(workingDirectory) {
   }
 
   this.config = new ConfigManager();
-  this.setWorkingDirectory(workingDirectory)
+  this.setWorkingDirectory(workingDirectory);
 
 };
 
@@ -28,9 +29,59 @@ PhonegapBoilerplate.prototype = {
 
   /**
    * Check that the cli is used in a phonegap boilerplate project
-   * @return {bool} true if we are in a pb project, else otherwise
+   * These points are checked :
+   * - presence of '/config.xml'
+   * - presence of a 'pb-core' branch pointing to the configured remote branch
+   * - presence of a '/.cordova' directory
+   * - presence of a '/www/cordova.js' file
+   * @param {function} done Called with err if we are not in a pb project
    */
-  checkWorkingDirectory: function() {
+  checkWorkingDirectory: function(doneChecking) {
+
+    var that = this;
+    doneChecking = doneChecking || function() {};
+
+    var checks = {
+      '/config.xml': function(done) {
+        that.checkPath(that.workingDirectory + '/config.xml', 'file', done);
+      },
+      'pb-core': function(done) {
+        done();
+      },
+      '/.cordova': function(done) {
+        that.checkPath(that.workingDirectory + '/.cordova', 'directory', done);
+      },
+      '/www/cordova.js': function(done) {
+        that.checkPath(that.workingDirectory + '/www/cordova.js', 'file', done);
+      },
+    };
+
+    async.parallel(checks, function(err) {
+      if (err) {
+        doneChecking(err);
+      } else {
+        doneChecking();
+      }
+    });
+  },
+
+  /**
+   * Test if a path exists and is a file or a directory
+   * @param {function} done Called with a potential error in parameter
+   */
+  checkPath: function(path, type, done) {
+    try {
+      var stats = fs.lstatSync(path);
+      if ((type === 'directory' && stats.isDirectory()) ||
+          (type === 'file' && stats.isFile())) {
+        done();
+      } else {
+        done(path + ' is not a ' + type + '.');
+      }
+    }
+    catch (err) {
+      done(err);
+    }
   },
 
   /**
@@ -43,24 +94,28 @@ PhonegapBoilerplate.prototype = {
    * Fetch from the pb-core remote
    */
   fetch: function() {
+    this.checkWorkingDirectory();
   },
 
   /**
    * Pull from the pb-core remote in the pb-core branch
    */
   update: function() {
+    this.checkWorkingDirectory();
   },
 
   /**
    * Merge the pb-core branch in the current branch
    */
   merge: function() {
+    this.checkWorkingDirectory();
   },
 
   /**
    * Push the current branch on the pb-core remote branch
    */
   push: function() {
+    this.checkWorkingDirectory();
   },
 };
 
